1e900bbef3648f2857cf6dd01c1935e1ace1f853
otrebosc <otrebosc@prosystem-nsf.com>
2019-09-23 09:57:08 +0200

    Try to use genericity

--- app/src/main/java/com/aklal/briquedagger2/MainPresenter.kt
+++ app/src/main/java/com/aklal/briquedagger2/MainPresenter.kt
@@ -1,14 +1,19 @@
 package com.aklal.briquedagger2
 
+import androidx.lifecycle.ViewModel
 import com.aklal.briquedagger2.Lapse.ChristmasTime
 import com.aklal.briquedagger2.Lapse.TIME
 import javax.inject.Inject
 
 class MainPresenter @Inject constructor(
-    private val mapOfLapse: Map<TIME, ChristmasTime>
+    private val mapOfLapse: Map<TIME, Class<out ChristmasTime>>
 ) : Presenter {
 
-    override fun getDateUntil(): String = mapOfLapse[TIME.UNTIL]?.getLapseOfTime() ?: "UNTIL FAILED"
+    override fun getDateUntil(): String {
+        val t: ChristmasTime = mapOfLapse[TIME.UNTIL] as ChristmasTime
+        return t.getLapseOfTime()
+    }
+
 
     //override fun getDateSince(): String = mapOfLapse[TIME.SINCE]?.getLapseOfTime() ?: "SINCE FAILED"
 }
